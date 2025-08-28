e39878a08265cedbe6aa02ee0845f2ce9a11793e
EduardoCoelho001 <eduardocoelho.edu@gmail.com>
2025-08-28 20:29:10 -0300

    Atualização 02
    Mudanças do footer, botão de contato, CSS, Pagina como adotar.
    
    Acréscimo de footer.js

---
+++ JS/footer.js
@@ -0,0 +1,7 @@
+// footer.js
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch(error => console.error("Erro ao carregar o footer:", error));
